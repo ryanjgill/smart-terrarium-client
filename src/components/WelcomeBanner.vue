9b9c25c1aa196c5bbe3dcd77414ee37654dfd700
Ryan Gill <ryanjgill2@gmail.com>
2019-05-24 12:48:00 -0500

    shrink banner on mobile

--- src/components/WelcomeBanner.vue
+++ src/components/WelcomeBanner.vue
@@ -1,12 +1,21 @@
 <template>
   <v-layout text-xs-center wrap>
     <v-flex>
-      <h1 class="display-4 font-weight-thin green--text text--darken-2">
-        Smart Terrarium
-      </h1>
-      <p class="display-1 grey--text text--darken-1 mb-4">
-        Monitor habitat to promote good husbandry.
-      </p>
+      <h1 class="display-4 font-weight-thin green--text text--darken-2">Smart Terrarium</h1>
+      <p class="display-1 grey--text text--darken-1 mb-4">Monitor habitat to promote good husbandry.</p>
     </v-flex>
   </v-layout>
 </template>
+
+<style lang="scss" scoped>
+@media screen and (max-width: 800px) {
+  .display-4 {
+    font-size: 4em !important;
+  }
+
+  p.display-1 {
+    font-size: 1.2em !important;
+  }
+}
+</style>
+
